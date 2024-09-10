0b73b629b26a82da2b5bcbf7a315120c735c6828
Joseph Wilson <josephcwilson47@gmail.com>
2024-09-09 21:20:41 -0500

    Added play pause buttons, still need to add rerendring for seek

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,27 +1,42 @@
 import { convertFileSrc } from '@tauri-apps/api/tauri';
 import { Howl } from 'howler';
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Controls = () => {
-  const currentTrack = '/home/joseph/Music/Humankind/1-08-Signs.mp3';
+  const [currentTrack, setCurrentTrack] = useState('/home/joseph/Music/Humankind/1-08-Signs.mp3');
+  const [playing, setPlaying] = useState(getHowl(currentTrack));
 
   function getAudioUrl(track_file: string) {
     return convertFileSrc(track_file);
   }
 
-  function playAudio() {
-    const assetUrl = getAudioUrl(currentTrack);
+  function getHowl(track: string): Howl {
+    const assetUrl = getAudioUrl(track);
     console.log(assetUrl);
-    const sound = new Howl({
+    return new Howl({
         src: [assetUrl]
     });
-
-    sound.play();
   }
+
+  function playSong() {
+    console.log(playing);
+    playing.play();
+  }
+
+
+  function pauseSong() {
+    console.log(playing);
+    console.log(playing.seek())
+    playing.pause();
+  }
+
 
   return (
     <div>
-      <button onClick={() => playAudio()}></button>
+      <button onClick={() => playSong()}>Play</button>
+      <button onClick={() => pauseSong()}>Pause</button>
+      <p>{playing.seek()}</p>
+      <p>{playing.duration()}</p>
     </div>
   );
 };
